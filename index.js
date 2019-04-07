d9cff36068ac7c8e9305cd1757637f957fad6fb9
Brian Tsai <briantsai2000@yahoo.com>
2019-04-06 20:58:39 -0700

    init commit
    init commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express')
+var path = require('path')
+var app = express();
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/views/index.html')
+})
+
+app.use(express.static('views'))
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
